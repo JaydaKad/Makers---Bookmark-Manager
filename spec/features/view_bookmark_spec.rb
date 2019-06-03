e37004a77553f2ce294e20a95a3619db1971ecbb
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-03 17:09:45 +0100

    Adds feature test for db integration

--- spec/features/view_bookmark_spec.rb
+++ spec/features/view_bookmark_spec.rb
@@ -1,9 +1,9 @@
 feature 'view all bookmarks' do
 
-
   scenario 'user can see bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content "bbc"
-    expect(page).to have_content "makersacademy"
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
