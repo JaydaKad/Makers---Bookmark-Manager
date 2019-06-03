e4434e9958c03591eceadc573cf7e467b642e61a
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-03 17:02:04 +0100

    Connects RB to PG - WIP

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,28 @@
+require 'pg'
+
 class Bookmark
   attr_reader :list
+
+  def initialize
+    @list = []
+  end
 
   def all
     @list = ['bbc', 'makersacademy']
   end
 
+  puts "*************"
+  begin 
+    con = PG.connect :dbname => 'bookmark_manager', :user => 'linna@paddle.com'
+    
+    rs = con.exec "SELECT * FROM bookmarks"
 
+    rs.each do |row|
+      item = "%s" % [row['url']]
+      @list.push(item)
+    end
+    puts "*************"
+  end
+
+  puts @list
 end
